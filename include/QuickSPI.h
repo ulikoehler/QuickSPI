15b7b7b307977d308666b2c45d682820fa949782
Uli Köhler <ukoehler@techoverflow.net>
2021-12-05 22:07:02 +0100

    Fix build issues

--- include/QuickSPI.h
+++ include/QuickSPI.h
@@ -10,26 +10,30 @@
  * 
  * DO NOT terminate these macro calls with a semicolon!
  */
-#define QUICKSPI_DEFINE_REGISTER8_RW(name, addr)\
-static constexpr uint8_t name = addr;\
-inline uint8_t read##name(uint8_t* out) {return read8BitRegister((addr));}\
-inline void write##name(uint8_t val) {return write8BitRegister((addr), val);}\
-inline bool writeAndVerify##name(uint8_t val) {return writeAndVerify8BitRegister((addr), val);}
-#define QUICKSPI_DEFINE_REGISTER16_RW(name, addr)\
-static constexpr uint8_t name = addr;\
-inline uint16_t read##name() {return read16BitRegister((addr));}\
-inline void write##name(uint16_t val) {return write16BitRegister((addr), val);}\
-inline bool writeAndVerify##name(uint16_t val) {return writeAndVerify16BitRegister((addr), val);}
-#define QUICKSPI_DEFINE_REGISTER24_RW(name, addr)\
-static constexpr uint8_t name = addr;\
-inline uint32_t read##name() {return read24BitRegister((addr));}\
-inline void write##name(uint32_t val) {return write24BitRegister((addr), val);}\
-inline bool writeAndVerify##name(uint32_t val) {return writeAndVerify24BitRegister((addr), val);}
-#define QUICKSPI_DEFINE_REGISTER32_RW(name, addr)\
-static constexpr uint8_t name = addr;\
-inline void read##name(uint32_t* out) {return read32BitRegister((addr));}\
-inline void write##name(uint32_t val) {return write32BitRegister((addr), val);}\
-inline bool writeAndVerify##name(uint32_t val) {return writeAndVerify32BitRegister((addr), val);}
+#define QUICKSPI_DEFINE_REGISTER8_RW(name, raddr, waddr)\
+static constexpr uint8_t name##ReadAddress = (raddr);\
+static constexpr uint8_t name##WriteAddress = (waddr);\
+inline uint8_t read##name(uint8_t* out) {return read8BitRegister((raddr));}\
+inline void write##name(uint8_t val) {return write8BitRegister((raddr), val);}\
+inline bool writeAndVerify##name(uint8_t val) {return writeAndVerify8BitRegister((raddr), (waddr), val);}
+#define QUICKSPI_DEFINE_REGISTER16_RW(name, raddr, waddr)\
+static constexpr uint8_t name##ReadAddress = (raddr);\
+static constexpr uint8_t name##WriteAddress = (waddr);\
+inline uint16_t read##name() {return read16BitRegister((raddr));}\
+inline void write##name(uint16_t val) {return write16BitRegister((waddr), val);}\
+inline bool writeAndVerify##name(uint16_t val) {return writeAndVerify16BitRegister((raddr), (waddr), val);}
+#define QUICKSPI_DEFINE_REGISTER24_RW(name, raddr, waddr)\
+static constexpr uint8_t name##ReadAddress = (raddr);\
+static constexpr uint8_t name##WriteAddress = (waddr);\
+inline uint32_t read##name() {return read24BitRegister((raddr));}\
+inline void write##name(uint32_t val) {return write24BitRegister((waddr), val);}\
+inline bool writeAndVerify##name(uint32_t val) {return writeAndVerify24BitRegister((raddr), (waddr), val);}
+#define QUICKSPI_DEFINE_REGISTER32_RW(name, raddr, waddr)\
+static constexpr uint8_t name##ReadAddress = (raddr);\
+static constexpr uint8_t name##WriteAddress = (waddr);\
+inline void read##name(uint32_t* out) {return read32BitRegister((raddr));}\
+inline void write##name(uint32_t val) {return write32BitRegister((waddr), val);}\
+inline bool writeAndVerify##name(uint32_t val) {return writeAndVerify32BitRegister((raddr), (waddr), val);}
 
 // Read-only register definitions
 #define QUICKSPI_DEFINE_REGISTER8_RO(name, addr)\
@@ -52,7 +56,10 @@ inline void read##name(uint32_t* out) {return read32BitRegister((addr));}
  * QuickSPIDevice will managed the clock speed, but it will not automagically manage the signal integrity!
  * You need to ensure that the slave select signals are never asserted accidentally and
  * when talking to a specific device, that the SCLK, MISO & MOSI signals are not distorted and
- * that the 
+ * that the delay from SCLK output to MISO input is not too long.
+ * 
+ * This class will perform dynamic memory allocation to allocate buffers.
+ * All dynamic memory will be freed before the respective QuickSPIDevice function exits.
  * 
  * Note: This class is not inherently thread-safe and does not perform locking by itself,
  * it inherits its thread safety from the SPI library.
@@ -64,6 +71,7 @@ public:
 
     uint8_t read8BitRegister(uint8_t registerAddress);
     uint16_t read16BitRegister(uint8_t registerAddress);
+    uint32_t read24BitRegister(uint8_t registerAddress);
     uint32_t read32BitRegister(uint8_t registerAddress);
     void readData(uint8_t registerAddress, uint8_t* buf, size_t len);
 
@@ -87,11 +95,11 @@ public:
      * @return true if the value which has been read back matches the value written into the device
      * @return false if the value written mismatches the value read back from the register
      */
-    bool writeAndVerify8BitRegister(uint8_t registerAddress, uint8_t value);
-    bool writeAndVerify16BitRegister(uint8_t registerAddress, uint16_t value);
-    bool writeAndVerify24BitRegister(uint8_t registerAddress, uint32_t value);
-    bool writeAndVerify32BitRegister(uint8_t registerAddress, uint32_t value);
-    bool writeAndVerifyData(uint8_t registerAddress, const uint8_t* buf, size_t len);
+    bool writeAndVerify8BitRegister(uint8_t readAddress, uint8_t writeAddress, uint8_t value);
+    bool writeAndVerify16BitRegister(uint8_t readAddress, uint8_t writeAddress, uint16_t value);
+    bool writeAndVerify24BitRegister(uint8_t readAddress, uint8_t writeAddress, uint32_t value);
+    bool writeAndVerify32BitRegister(uint8_t readAddress, uint8_t writeAddress, uint32_t value);
+    bool writeAndVerifyData(uint8_t readAddress,uint8_t writeAddress, const uint8_t* buf, size_t len);
 
     /**
      * Milliseconds delay() between write and read during a writeAndVerifyData()
@@ -102,5 +110,5 @@ public:
 protected:
     SPIClass& spi;
     uint8_t ssPin;
-    SPISettings settings;
+    SPISettings spiSettings;
 };
--- src/QuickSPI.cpp
+++ src/QuickSPI.cpp
@@ -8,15 +8,15 @@ void QuickSPIDevice::writeData(uint8_t registerAddress, const uint8_t* buf, size
     // Length + 1 since we need to 
     // NOTE: Received data will
     uint8_t* trxbuf = new uint8_t[len + 1];
-    txbuf[0] = registerAddress;
+    trxbuf[0] = registerAddress;
     // Copy source data
     memcpy(trxbuf + 1, buf, len);
     // SPI transaction
-    spi->beginTransaction(spiSettings);
+    spi.beginTransaction(spiSettings);
     digitalWrite(ssPin, LOW);
-    spi->transfer(trxbuf, len + 1);
+    spi.transfer(trxbuf, len + 1);
     digitalWrite(ssPin, HIGH);
-    spi->endTransaction();
+    spi.endTransaction();
     // Cleanup. Discards received data
     delete[] trxbuf;
 }
@@ -26,47 +26,47 @@ void QuickSPIDevice::writeAndReceiveData(uint8_t registerAddress, uint8_t* buf, 
     // Length + 1 since we need to 
     // NOTE: Received data will
     uint8_t* trxbuf = new uint8_t[len + 1];
-    txbuf[0] = registerAddress;
+    trxbuf[0] = registerAddress;
     // Copy source data
     memcpy(trxbuf + 1, buf, len);
     // SPI transaction
-    spi->beginTransaction(spiSettings);
+    spi.beginTransaction(spiSettings);
     digitalWrite(ssPin, LOW);
-    spi->transfer(trxbuf, len + 1);
+    spi.transfer(trxbuf, len + 1);
     digitalWrite(ssPin, HIGH);
-    spi->endTransaction();
+    spi.endTransaction();
     // Copy received data
     memcpy(buf, trxbuf + 1, len);
     // Cleanup.
     delete[] trxbuf;
 }
 
-uint8_t void QuickSPIDevice::readData(uint8_t registerAddress, uint8_t* buf, size_t len) {
+void QuickSPIDevice::readData(uint8_t registerAddress, uint8_t* buf, size_t len) {
     // Transmit & receive buffer.
     // Length + 1 since we need to 
     // NOTE: Received data will
     uint8_t* trxbuf = new uint8_t[len + 1];
-    txbuf[0] = registerAddress;
+    trxbuf[0] = registerAddress;
     // SPI transaction
-    spi->beginTransaction(spiSettings);
+    spi.beginTransaction(spiSettings);
     digitalWrite(ssPin, LOW);
-    spi->transfer(trxbuf, len + 1);
+    spi.transfer(trxbuf, len + 1);
     digitalWrite(ssPin, HIGH);
-    spi->endTransaction();
+    spi.endTransaction();
     // Copy to destination buffer
     memcpy(buf, trxbuf + 1, len);
     // Cleanup
     delete[] trxbuf;
 }
 
-bool QuickSPIDevice::writeAndVerifyData(uint8_t registerAddress, const uint8_t* buf, size_t len) {
+bool QuickSPIDevice::writeAndVerifyData(uint8_t readAddress, uint8_t writeAddress, const uint8_t* buf, size_t len) {
     // Try to write - if it fails, do not try to verify
-    writeData(registerAddress, buf, len);
+    writeData(writeAddress, buf, len);
     // Insert grace time between write and read
     delay(delayBetweenWriteAndRead);
     // Read back data for verify
     uint8_t* rxbuf = new uint8_t[len];
-    readData(registerAddress, rxbuf, len);
+    readData(readAddress, rxbuf, len);
     // Compare data
     bool result = memcmp(rxbuf, buf, len) == 0; // true => rx data is the same as tx data
     // cleanup
@@ -74,31 +74,39 @@ bool QuickSPIDevice::writeAndVerifyData(uint8_t registerAddress, const uint8_t* 
     return result;
 }
 
-void QuickSPIDevice::read8BitRegister(uint8_t registerAddress) {
-    return readData(registerAddress, (uint8_t*)buf, 1);
+uint8_t QuickSPIDevice::read8BitRegister(uint8_t registerAddress) {
+    uint8_t ret;
+    readData(registerAddress, (uint8_t*)&ret, 1);
+    return ret;
 }
 
-void QuickSPIDevice::read16BitRegister(uint8_t registerAddress) {
-    return readData(registerAddress, (uint8_t*)buf, 2);
+uint16_t QuickSPIDevice::read16BitRegister(uint8_t registerAddress) {
+    uint16_t ret;
+    readData(registerAddress, (uint8_t*)&ret, 2);
+    return ret;
 }
 
-void QuickSPIDevice::read24BitRegister(uint8_t registerAddress) {
-    return readData(registerAddress, (uint8_t*)buf, 3);
+uint32_t QuickSPIDevice::read24BitRegister(uint8_t registerAddress) {
+    uint32_t ret;
+    readData(registerAddress, (uint8_t*)&ret, 3);
+    return ret;
 }
 
-void QuickSPIDevice::read32BitRegister(uint8_t registerAddress, uint32_t* buf) {
-    return readData(registerAddress, (uint8_t*)buf, 4);
+uint32_t QuickSPIDevice::read32BitRegister(uint8_t registerAddress) {
+    uint32_t ret;
+    readData(registerAddress, (uint8_t*)&ret, 4);
+    return ret;
 }
 
 void QuickSPIDevice::write8BitRegister(uint8_t registerAddress, uint8_t value) {
-    return writeData(registerAddress, &value, 1);
+    return writeData(registerAddress, (uint8_t*)&value, 1);
 }
 
 void QuickSPIDevice::write16BitRegister(uint8_t registerAddress, uint16_t value) {
     return writeData(registerAddress, (uint8_t*)&value, 2);
 }
 
-void QuickSPIDevice::write24BitRegister(uint8_t registerAddress, uint16_t value) {
+void QuickSPIDevice::write24BitRegister(uint8_t registerAddress, uint32_t value) {
     return writeData(registerAddress, (uint8_t*)&value, 3);
 }
 
@@ -106,27 +114,18 @@ void QuickSPIDevice::write32BitRegister(uint8_t registerAddress, uint32_t value)
     return writeData(registerAddress, (uint8_t*)&value, 4);
 }
 
-void QuickSPIDevice::writeAndVerify8BitRegister(uint8_t registerAddress, uint8_t value, uint8_t* rxbuf) {
-    return writeAndVerifyData(registerAddress, &value, 1, rxbuf);
+bool QuickSPIDevice::writeAndVerify8BitRegister(uint8_t readAddress, uint8_t writeAddress, uint8_t value) {
+    return writeAndVerifyData(readAddress, writeAddress, (uint8_t*)&value, 1);
 }
 
-void QuickSPIDevice::writeAndVerify16BitRegister(uint8_t registerAddress, uint16_t value, uint8_t* rxbuf) {
-    return writeAndVerifyData(registerAddress, (uint8_t*)&value, 2, rxbuf);
+bool QuickSPIDevice::writeAndVerify16BitRegister(uint8_t readAddress, uint8_t writeAddress, uint16_t value) {
+    return writeAndVerifyData(readAddress, writeAddress, (uint8_t*)&value, 2);
 }
 
-void QuickSPIDevice::writeAndVerify24BitRegister(uint8_t registerAddress, uint32_t value, uint8_t* rxbuf) {
-    return writeAndVerifyData(registerAddress, (uint8_t*)&value, 3, rxbuf);
+bool QuickSPIDevice::writeAndVerify24BitRegister(uint8_t readAddress, uint8_t writeAddress, uint32_t value) {
+    return writeAndVerifyData(readAddress, writeAddress, (uint8_t*)&value, 3);
 }
 
-void QuickSPIDevice::writeAndVerify32BitRegister(uint8_t registerAddress, uint32_t value, uint8_t* rxbuf) {
-    return writeAndVerifyData(registerAddress, (uint8_t*)&value, 4, rxbuf);
-}
-
-
-uint32_t QuickSPIDevice::computeTimeout(uint32_t bytesToTransfer) {
-    uint32_t numBits = bytesToTransfer * 9; // 8 data bits + 1 ACK/NACK bit per byte
-    // NOTE: We avoid to use floating point arithmetic here.
-    uint32_t durationMilliseconds = numBits * 1000 / this->SPIClockSpeed; // This will ALWAYS be rounded down!
-    // Give an extra millisecond so we have ensured to always round up
-    return durationMilliseconds + 2;
+bool QuickSPIDevice::writeAndVerify32BitRegister(uint8_t readAddress, uint8_t writeAddress, uint32_t value) {
+    return writeAndVerifyData(readAddress, writeAddress, (uint8_t*)&value, 4);
 }
