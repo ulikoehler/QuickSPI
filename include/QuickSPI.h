7d6a678ffcd0ecd0ae3a6e9c670722baa7e75edd
Uli Köhler <ukoehler@techoverflow.net>
2024-05-09 03:26:49 +0200

    Add possibility to hide utils

--- include/QuickSPI.h
+++ include/QuickSPI.h
@@ -99,6 +99,9 @@ constexpr typename std::enable_if<std::is_enum<T>::value && std::is_integral<I>:
 
 #endif
 
+#if !defined(QUICKI2C_UTILS)
+#define QUICKSPI_UTILS
+
 // Pre/postprocess functions that do nothing
 inline uint8_t noop(uint8_t address, uint8_t rawValue) {return rawValue;}
 inline uint16_t noop(uint8_t address, uint16_t rawValue) {return rawValue;}
@@ -108,6 +111,8 @@ inline uint32_t noop(uint8_t address, uint32_t rawValue) {return rawValue;}
 inline uint16_t invertByteorder16(uint8_t address, uint16_t rawValue) {return __builtin_bswap16(rawValue);}
 inline uint32_t invertByteorder24(uint8_t address, uint32_t rawValue) {return __builtin_bswap32(rawValue) >> 8;}
 inline uint32_t invertByteorder32(uint8_t address, uint32_t rawValue) {return __builtin_bswap32(rawValue);}
+
+#endif
 
 typedef uint8_t (*Postprocessor8Bit)(uint8_t, uint8_t);
 typedef uint16_t (*Postprocessor16Bit)(uint8_t, uint16_t);
